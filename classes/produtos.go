454ae4a9b3ce6cde68c85fd0e81183f877528160
Bremenkamp <bremenkjoaopaulo@gmail.com>
2025-10-03 14:36:31 -0300

    "iniciando estrutura do projeto"

---
+++ classes/produtos.go
@@ -0,0 +1,10 @@
+package classes
+
+type Produtos struct {
+	nome              string
+	descricao         string
+	preco             float32
+	quantidadeEstoque int
+}
+
+func AddProdutos(nome string, descricao string)
